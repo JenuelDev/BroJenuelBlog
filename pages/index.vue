8524279f7dbf307c02d76f0d09dcca3179a42648
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-04-02 10:56:15 +0800

    updates
    sdfsdf

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mr-auto ml-auto dark:bg-gray-800">
+        <Header />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
