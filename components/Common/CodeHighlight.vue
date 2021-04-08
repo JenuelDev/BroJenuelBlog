339a61794c6ced69bed350d3dbaaeef15c603af3
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-04-08 11:04:09 +0800

    updates

---
+++ components/Common/CodeHighlight.vue
@@ -0,0 +1,21 @@
+<template>
+    <no-ssr>
+    <div class="flex justify-center" style="width: 100% !important">
+        <vue-code-highlight language="javascript">
+            <pre>
+            <slot />
+            </pre>
+        </vue-code-highlight>
+    </div>
+    </no-ssr>
+</template>
+<script>
+import { component as VueCodeHighlight } from 'vue-code-highlight';
+
+export default {
+    components:{
+        VueCodeHighlight
+        }
+}
+
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -26,12 +26,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/styles/main.scss'
+    '~/assets/styles/main.scss',
+    'vue-code-highlight/themes/window.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
