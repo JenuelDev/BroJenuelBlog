087b781a6d40daaf453e13bf2ef758e8913094bd
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-04-26 20:05:22 +0800

    update

--- dist/_nuxt/static/1619437970/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fview\u002Fmy-first-blog","\u002Fabout","\u002Fcontact","\u002Fview\u002Fmaking-own-portfolio","\u002F","\u002Fview\u002Fnuxt-pwa-update","\u002Fview\u002Fjavascript-tips-tricks-n1"]})
