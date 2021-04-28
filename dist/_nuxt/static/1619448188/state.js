f124c7d29ec0011b4421b5ef7dac5578a4c44182
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-04-28 09:18:12 +0800

    updates

--- dist/_nuxt/static/1619448188/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619448188",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fbde6666"}}}}(null,"\u002F"));
