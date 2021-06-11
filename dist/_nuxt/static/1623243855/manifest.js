b4526a332d386620c9004e32d4dae337f2547ddd
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-06-11 10:56:38 +0800

    removed dist and update seo generator

--- dist/_nuxt/static/1623243855/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fview\u002Fjavascript-tips-tricks-n1","\u002Fview\u002Fhow-to-say-no-to-client","\u002Fcontact","\u002Fview\u002Fwhy-devs-want-to-learn-vue","\u002Fview\u002Fmaking-own-portfolio","\u002Fview\u002Fmy-first-blog","\u002Fview\u002Fnuxt-pwa-update"]})
--- utils/seo.ts
+++ utils/seo.ts
@@ -1,21 +1,34 @@
 export const createSEOMeta = (data: any) => [
-    { hid: 'og:title', property: 'og:title', content: data.title },
+    { hid: 'title', property: 'title', content: data.title },
     { hid: 'description', name: 'description', content: data.description },
     { hid: 'image', property: 'image', content: data.image },
     {
-      hid: 'og:description',
-      property: 'og:description',
-      content: data.description,
+      hid: 'url',
+      property: 'url',
+      content: data.url,
+  },
+
+    // OGs
+    { hid: 'og:title', property: 'og:title', content: data.title },
+    {
+        hid: 'og:description',
+        property: 'og:description',
+        content: data.description,
     },
     { hid: 'og:image', property: 'og:image', content: data.image },
+    { hid: 'og:image:alt', property: 'og:image:alt', content: 'Cover Image' },
     {
-      hid: 'og:url',
-      property: 'og:url',
-      content: process.env.HOST_NAME + '/' + data.url,
+        hid: 'og:url',
+        property: 'og:url',
+        content: data.url,
     },
+
+    // twitter
+    { hid: 'twitter:title', property: 'twitter:title', content: data.title },
+    { hid: 'twitter:creator', property: 'twitter:creator', content: '@BroJenuel' },
     {
-      hid: 'twitter:card',
-      name: 'twitter:card',
-      content: data.cardType || 'summary_large_image',
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: data.cardType || 'summary_large_image',
     },
 ]
