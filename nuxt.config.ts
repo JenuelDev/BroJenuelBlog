925838b7e3a11ac64d06b749483944ed7a2caf6c
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-09-24 13:36:32 +0800

    updates added api route to get recent posts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
+import { serverQueryContent } from "#content/server";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ["@nuxt/content", "nuxt-windicss"],
+    modules: ["@nuxt/content", "@nuxtjs/feed", "nuxt-windicss"],
     css: ["@/assets/styles/main.scss"],
     nitro: {
         prerender: {
@@ -11,5 +13,27 @@ export default defineNuxtConfig({
         highlight: {
             theme: "one-dark-pro",
         },
+    },
+    feed() {
+        // const articles = await
+
+        const { $content } = require("@nuxt/content");
+
+        const createFeedArticles = async function () {
+            // const docs = await serverQueryContent().find();
+            // console.log(docs);
+        };
+
+        createFeedArticles();
+
+        return [
+            {
+                path: "/feed.xml", // The route to your feed.
+                async create(feed) {}, // The create function (see below)
+                cacheTime: 1000 * 60 * 15, // How long should the feed be cached
+                type: "rss2", // Can be: rss2, atom1, json1
+                data: ["Some additional data"], // Will be passed as 2nd argument to `create` function
+            },
+        ];
     },
 });
