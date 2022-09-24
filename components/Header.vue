9693ca8d1579ce0023bd08025342e9bc93c4a327
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-09-24 15:12:29 +0800

    Added a donate button o nthe top right in the nav

--- components/Header.vue
+++ components/Header.vue
@@ -15,7 +15,7 @@
                     <span class="text-size-1.5rem !font-100">BroJenuel</span>
                 </div>
             </h2>
-            <div class="flex gap-20px text-size-16px">
+            <div class="flex gap-20px text-size-16px items-center">
                 <NuxtLink
                     to="/"
                     class="hover:text-[var(--primary)] hover:underline"
@@ -27,6 +27,22 @@
                     class="hover:text-[var(--primary)] hover:underline"
                 >
                     <span class="font-500"> BroJenuel </span>
+                </a>
+                <a
+                    href="https://ko-fi.com/T6T5379QZ"
+                    onclick="window.open('https://ko-fi.com/T6T5379QZ', 
+                        'newwindow', 
+                        'width=1000,height=800'); 
+                        return false;"
+                    target="_blank"
+                >
+                    <img
+                        height="36"
+                        style="border: 0px; height: 36px"
+                        src="https://cdn.ko-fi.com/cdn/kofi3.png?v=3"
+                        border="0"
+                        alt="Buy Me a Coffee at ko-fi.com"
+                    />
                 </a>
             </div>
         </div>
