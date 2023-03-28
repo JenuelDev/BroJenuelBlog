30759d4a20102339ddd8cb7f6a67262b7dd5b45a
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-03-29 07:04:17 +0800

    Update AboutMe.vue

--- components/Home/AboutMe.vue
+++ components/Home/AboutMe.vue
@@ -3,17 +3,14 @@ const currentDate = new Date();
 </script>
 <template>
     <div id="about-me">
-        <div class="font-800 text-size-20px text-[var(--primary)] w-full max-w-500px mx-auto px-20px pt-80px">
-            About Me
-        </div>
+        <div class="font-800 text-size-20px text-[var(--primary)] w-full max-w-500px mx-auto px-20px pt-80px">About Me</div>
         <div class="pt-5 w-full max-w-550px mx-auto px-20px">
             <div class="pb-3">
-                My name is Jenuel Ganawed or call me BroJenuel. I’m a developer with a full-stack background and a keen
-                eye for good design. Currently, I focus on building apps and software using web tools.
+                I'm a software engineer from Luzon Philippines. I'm passionate about coding, writing, and sharing what I know. Welcome to my home on the internet!
             </div>
             <div>
-                Over the past {{ currentDate.getFullYear() - 2017 }} years, I’ve gained experience across some aspects
-                of the application lifecycle, including frontend, backend, and dev-ops.
+                This site is my personal portfolio website were I can share my experiences and some personal projects and On this site I write about code, self-improvement, health,
+                and more.
             </div>
         </div>
     </div>
