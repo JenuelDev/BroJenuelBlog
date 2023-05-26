9d59dcdc5c2171c0f267dc12c37deb19c826c288
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-05-26 19:19:02 +0800

    Updates

--- components/Drop/Menu.vue
+++ components/Drop/Menu.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
 type MenuTypeItem = { label: string; key: string; icon: string };
 type MenuTYPE = Array<MenuTypeItem>;
-
 const showMenu = ref(false);
 const dropdownRef = ref(null);
 const emit = defineEmits(["change", "update:modelValue"]);
-const props = defineProps(["menuOptions", "label", "modelValue"]);
-const valueKey = ref<string | null>(null);
-const menus: MenuTYPE = props.menuOptions;
+const props = defineProps({
+    menuOptions: Array,
+    label: { type: String, default: "Label" },
+    modelValue: String,
+    position: {
+        type: String,
+        default: "left",
+        validator(v: string) {
+            return ["left", "right"].includes(v);
+        },
+    },
+});
+const menus = props.menuOptions as MenuTYPE;
 const LABEL = computed(() => {
     const findIndex = menus.findIndex((menu) => menu.key == valueKey.value);
     if (findIndex > -1) return menus[findIndex].label;
     return null;
 });
 
-onMounted(() => {
-    if (props.modelValue) valueKey.value = props.modelValue;
+const valueKey = computed({
+    get() {
+        return props.modelValue;
+    },
+    set(value) {
+        emit("update:modelValue", value);
+    },
 });
 
 function selectKey(menu: MenuTypeItem) {
@@ -29,7 +43,7 @@ function selectKey(menu: MenuTypeItem) {
         valueKey.value = menu.key;
     } else {
         r.searchParams.delete("cat");
-        valueKey.value = null;
+        valueKey.value = undefined;
     }
 
     const newUrl = r.href;
@@ -52,16 +66,20 @@ onClickOutside(dropdownRef, () => (showMenu.value = showMenu.value == true ? fal
             @click="showMenu = !showMenu"
             :class="{ 'text-[var(--primary)]': showMenu }"
         >
-            <span>{{ valueKey ? LABEL : label ?? "Label" }}</span>
+            <span :class="{ 'text-[var(--primary)]': LABEL }">{{ valueKey ? LABEL ?? label : label ?? "Label" }}</span>
             <Icon class="transition-all" :class="{ 'transform rotate-90': showMenu }" name="ic:outline-play-arrow" />
         </div>
 
         <Transition name="drop-menu">
             <div
                 v-show="showMenu"
-                class="dropdown-menu absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[var(--background)] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-99 select-none"
+                class="dropdown-menu absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-[var(--background-secondary-opacity-100)] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-99 select-none"
+                :class="{ 'right-0': props.position == 'left', 'left-0': props.position == 'right' }"
             >
                 <div class="py-1" role="none">
+                    <small class="pl-4">
+                        {{ label }}
+                    </small>
                     <div
                         v-for="menu in menus"
                         :key="menu.key"
