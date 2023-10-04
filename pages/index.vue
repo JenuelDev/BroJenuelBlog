085042b85c57c566a6c6f86df632b7d80d5934c8
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-10-04 21:10:02 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,6 @@ useHead({
 });
 </script>
 <template>
-    <OgImageStatic
-        :description="`Jenuel Ganawed is a software developer from the philippines. His passionate about coding, writing, and sharing what he know. Sharing about experiences, personal projects and articles about code, self-improvement, health, and more`"
-        :path="route.path"
-        :title="`BroJenuel`"
-        appName="www.BroJenuel.com"
-        component="DefaultOgImage"
-    />
     <NuxtLayout>
         <section class="items-center justify-center pt-20px relative sm:mt-100px mt-50px pb-100px">
             <div
