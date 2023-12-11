c5249ef61c8fcf84b704d4a7f529119d59e2d048
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-12-11 18:20:34 +0800

    Update

--- components/Home/HeroContent.vue
+++ components/Home/HeroContent.vue
@@ -1,106 +1,54 @@
 <template>
     <div
-        class="flex gap-2 w-full max-w-1100px mx-auto md:px-20px px-10px justify-center md:flex-row flex-col"
+        class="flex gap-2 w-full max-w-700px mx-auto md:px-20px px-10px justify-center md:flex-row flex-col items-center"
         style="margin-top: 90px"
     >
-        <div class="md:w-[30%] self-auto w-full z-9 flex flex-col gap-2 md:items-start items-center justify-center">
-            <div class="lg:text-lg md:text-left text-center">
-                <div>
-                    Hi! <span class="animate-wave lg:text-size-44px md:text-size-36px text-size-30px">👋</span> <br />
-                    Im <b>Jenuel</b> & I am a
-                </div>
-            </div>
-            <div class="md:text-left text-center">
-                <h2 class="lg:text-5xl text-4xl w-auto rounded-1xl">Software</h2>
-                <h2
-                    class="font-900 text-4xl whitespace-nowrap lg:w-270px w-full rounded-br-1xl relative h-50px md:-ml-0px -ml-65px"
-                >
-                    <div class="animation absolute">
-                        <div class="first"><div>Engineer</div></div>
-                        <div class="second"><div>Programmer</div></div>
-                        <div class="third"><div>Developer</div></div>
-                    </div>
-                </h2>
-            </div>
-            <div class="h-1px dark:bg-light-50 bg-dark-50 w-60px self-center md:my-0 my-3"></div>
-            <div class="lg:text-lg md:text-left text-center">
-                i programme to develop apps and websites, I create articles, and I love what I do.
-            </div>
-        </div>
-        <div>
+        <div class="m-3 rounded-full bg-gradient-to-r from-green-400 via-blue-400 to-purple-700 p-1 text-white">
             <div
-                class="light:bg-[#dee2e3] bg-[var(--primary)] md:w-360px sm:w-350px w-300px rounded-2xl z-8 md:h-360px sm:h-350px h-300px self-center overflow-hidden group relative mx-auto group"
+                class="rounded-full bg-[var(--background)] p-4 overflow-hidden h-100px w-100px flex items-center justify-center border border-3px border-light-50"
             >
-                <div
-                    class="absolute right-5 top-5 h-4 w-4 bg-gray-50 opacity-50 rounded-full group-hover:h-6 group-hover:w-6 group-hover:top-3 group-hover:right-3 transition-all duration-500"
-                ></div>
-                <div
-                    class="absolute right-5 top-11 h-4 w-4 bg-gray-50 opacity-60 rounded-full group-hover:top-15 transition-all duration-500"
-                ></div>
-                <div
-                    class="absolute right-11 top-5 h-4 w-4 bg-gray-50 opacity-60 rounded-full group-hover:right-15 transition-all duration-500"
-                ></div>
                 <NuxtImg
                     src="/my-image/smile face.png"
                     format="webp"
-                    width="300"
-                    height="300"
-                    quality="80"
-                    class="group-hover:block w-full absolute group-hover:transform group-hover:scale-110 transition-all"
+                    width="190"
+                    height="190"
+                    quality="100"
                     alt="brojenuel profile"
                 />
-                <div
-                    class="absolute left-5 bottom-5 h-4 w-4 bg-gray-50 opacity-50 rounded-full group-hover:h-6 group-hover:w-6 transition-all duration-500"
-                ></div>
-                <div
-                    class="absolute left-5 bottom-11 h-4 w-4 bg-gray-50 opacity-60 rounded-full group-hover:bottom-15 transition-all duration-500"
-                ></div>
-                <div
-                    class="absolute left-11 bottom-5 h-4 w-4 bg-gray-50 opacity-60 rounded-full group-hover:left-15 transition-all duration-500"
-                ></div>
             </div>
         </div>
-    </div>
-    <div
-        class="flex gap-2 w-full max-w-1100px mx-auto md:px-20px px-10px justify-center md:flex-row flex-col py-5 pt-15"
-    >
-        <div class="w-full px-5 py-3 text-center">
-            <div class="sm:text-lg max-w-700px mx-auto leading-8 mb-5">
-                Since the beginning of my journey as a software developer 6 years ago, I've done office and remote work
-                for companies, and collaborated with teams of developers to build and maintain awesome products. I'm
-                quietly confident, naturally curious, and perpetually working on improving my craft.
-            </div>
-            <div class="flex items-center gap-7px flex-wrap justify-center">
-                <a
+        <div>
+            <p class="text-3xl ml-4 font-bold dark:text-gray-400 text-gray-500">
+                <NuxtLink to="/" class="text-[var(--color)] decoration-none hover:underline"> Hey, Im Jenuel </NuxtLink>
+                . I'm a software developer and a project lead.
+            </p>
+            <div class="flex items-center gap-5 flex-wrap">
+                <NuxtLink
                     href="https://dev.to/brojenuel"
-                    class="flex items-center hover:text-[var(--primary)]"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg"
                     target="_blank"
                     alt="Jenuel Oras Ganawed Dev"
                     aria-label="Jenuel Oras Ganawed Dev"
                 >
-                    <span class="icon--mdi icon--mdi--dev-to text-70px"></span>
-                </a>
-                <a
+                    <span class="icon--mdi icon--mdi--dev-to text-40px"></span>
+                    Dev.to
+                </NuxtLink>
+                <NuxtLink
                     href="https://www.linkedin.com/in/jenuelganawed/"
-                    class="flex items-center hover:text-[var(--primary)]"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg"
                     target="_blank"
                     alt="Jenuel Oras Ganawed Linkedin"
                     aria-label="Jenuel Oras Ganawed Linkedin"
                 >
-                    <span class="icon--mdi icon--mdi--linkedin text-50px"></span>
-                </a>
-                <NuxtLink
-                    to="/cv"
-                    class="border px-5 py-2 rounded-full flex items-center gap-1 hover:text-[var(--primary)] !hover:border-[var(--primary)] hover:underline dark:border-white"
-                >
-                    <span class="icon--solar icon--solar--document-broken"></span>
+                    <span class="icon--mdi icon--mdi--linkedin text-30px"></span>
+                    LinkedIn
+                </NuxtLink>
+                <NuxtLink to="/cv" class="flex items-center text-[var(--color)] decoration-none text-lg">
+                    <span class="icon--solar icon--solar--document-broken text-30px"></span>
                     Resume
                 </NuxtLink>
-                <NuxtLink
-                    to="/contact"
-                    class="border border-gray-900 px-5 py-2 bg-gray-900 text-[var(--background)] rounded-full flex items-center gap-2 hover:underline dark:bg-gray-50"
-                >
-                    <span class="icon--solar icon--solar--mailbox-broken"></span>
+                <NuxtLink to="/contact" class="flex items-center text-[var(--color)] decoration-none text-lg">
+                    <span class="icon--solar icon--solar--mailbox-broken text-30px"></span>
                     Email Me
                 </NuxtLink>
             </div>
