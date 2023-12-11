b9c16f800541922986e9baaaa868d4438da9ee5c
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-12-11 18:33:50 +0800

    Update the hero header to make it very sim

--- components/Home/HeroContent.vue
+++ components/Home/HeroContent.vue
@@ -1,11 +1,10 @@
 <template>
     <div
-        class="flex gap-2 w-full max-w-700px mx-auto md:px-20px px-10px justify-center md:flex-row flex-col items-center"
-        style="margin-top: 90px"
+        class="flex gap-2 w-full max-w-700px mx-auto md:px-20px px-10px justify-center md:flex-row flex-col items-center mt-30 mb-10"
     >
         <div class="m-3 rounded-full bg-gradient-to-r from-green-400 via-blue-400 to-purple-700 p-1 text-white">
             <div
-                class="rounded-full bg-[var(--background)] p-4 overflow-hidden h-100px w-100px flex items-center justify-center border border-3px border-light-50"
+                class="rounded-full dark:bg-gray-50 bg-gray-900 p-4 overflow-hidden h-100px w-100px flex items-center justify-center border border-3px border-light-50"
             >
                 <NuxtImg
                     src="/my-image/smile face.png"
@@ -18,14 +17,14 @@
             </div>
         </div>
         <div>
-            <p class="text-3xl ml-4 font-bold dark:text-gray-400 text-gray-500">
+            <p class="text-3xl m-1 font-bold dark:text-gray-400 text-gray-500">
                 <NuxtLink to="/" class="text-[var(--color)] decoration-none hover:underline"> Hey, Im Jenuel </NuxtLink>
                 . I'm a software developer and a project lead.
             </p>
             <div class="flex items-center gap-5 flex-wrap">
                 <NuxtLink
                     href="https://dev.to/brojenuel"
-                    class="flex items-center text-[var(--color)] decoration-none text-lg"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg gap-1 hover:text-gray-500"
                     target="_blank"
                     alt="Jenuel Oras Ganawed Dev"
                     aria-label="Jenuel Oras Ganawed Dev"
@@ -35,20 +34,26 @@
                 </NuxtLink>
                 <NuxtLink
                     href="https://www.linkedin.com/in/jenuelganawed/"
-                    class="flex items-center text-[var(--color)] decoration-none text-lg"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg gap-1 hover:text-[#337DAF]"
                     target="_blank"
                     alt="Jenuel Oras Ganawed Linkedin"
                     aria-label="Jenuel Oras Ganawed Linkedin"
                 >
-                    <span class="icon--mdi icon--mdi--linkedin text-30px"></span>
+                    <span class="icon--mdi icon--mdi--linkedin text-30px text-[#337DAF]"></span>
                     LinkedIn
                 </NuxtLink>
-                <NuxtLink to="/cv" class="flex items-center text-[var(--color)] decoration-none text-lg">
-                    <span class="icon--solar icon--solar--document-broken text-30px"></span>
-                    Resume
+                <NuxtLink
+                    to="/cv"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg gap-1 hover:text-[#EF2527]"
+                >
+                    <span class="icon--mdi icon--mdi--youtube text-30px text-[#EF2527]"></span>
+                    Youtube
                 </NuxtLink>
-                <NuxtLink to="/contact" class="flex items-center text-[var(--color)] decoration-none text-lg">
-                    <span class="icon--solar icon--solar--mailbox-broken text-30px"></span>
+                <NuxtLink
+                    to="/contact"
+                    class="flex items-center text-[var(--color)] decoration-none text-lg gap-1 hover:text-[#32aa56]"
+                >
+                    <span class="icon--solar icon--solar--mailbox-broken text-30px text-[#32aa56]"></span>
                     Email Me
                 </NuxtLink>
             </div>
